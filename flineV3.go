83564e8294f316113b308aa532578b6766c33a40
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2024-10-26 16:47:42 +0200

    upgraded tests

--- flineV3.go
+++ flineV3.go
@@ -38,6 +38,9 @@ func main() {
 	testLines = append(testLines, []byte("GET / http/1.1\r\n"))
 	testLines = append(testLines, []byte("GET /index http/1.1\n"))
 	testLines = append(testLines, []byte("GET /index http/1.1\r\n"))
+	testLines = append(testLines, []byte("GET index http/1.1\r\n"))
+	testLines = append(testLines, []byte("GET http://azulweb/index http/1.1\r\n"))
+	testLines = append(testLines, []byte("GET https://azulweb/index/testfile.dat http/1.1\r\n"))
 
 	fmt.Printf("*** testLines: %d ****\n", len(testLines))
 	for i:= 0; i<len(testLines); i++ {
@@ -50,6 +53,26 @@ func main() {
 			pars.PrintRes(res)
 		}
 	}
+
+	testLines2 := make([]SL, 0, 10)
+	testLines2 = append(testLines2, []byte("GET /index?key1=val1 http/1.1\r\n"))
+	testLines2 = append(testLines2, []byte("GET /index?key1=val1&key2=val2 http/1.1\r\n"))
+	testLines2 = append(testLines2, []byte("GET /index?key1=val1&key2=val2#anchor http/1.1\r\n"))
+	testLines2 = append(testLines2, []byte("GET /index#anchor http/1.1\r\n"))
+//	testLines2 = append(testLines2, []byte("GET /index http/1.1\r\n"))
+
+	fmt.Printf("*** testLines2: %d ****\n", len(testLines))
+	for i:= 0; i<len(testLines2); i++ {
+		inp:= testLines2[i]
+		fmt.Printf("\nline[%d]: %s", i, inp)
+		res, err := pars.ParseFLHttp(inp)
+		if err != nil {
+			fmt.Printf("error -- line: %d ->parseFLHttp: %v\n",i, err)
+		} else {
+			pars.PrintRes(res)
+		}
+	}
+
 	fmt.Println("*** success ***")
 }
 
